7888ebc6792b34aa3b4a5640a665b83f1db14803
0xd18b <70670395+0xd18b@users.noreply.github.com>
2021-07-30 14:42:39 -0400

    bootstrap ipfs gateway

---
+++ pulp-ipfs-gateway/src/modules/admin/access/permissions/dtos/update-permission-request.dto.ts
@@ -0,0 +1,11 @@
+import { CreatePermissionRequestDto } from './create-permission-request.dto';
+import { IsBoolean, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdatePermissionRequestDto extends CreatePermissionRequestDto {
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsBoolean()
+    active: boolean;
+}
